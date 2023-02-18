3bec3cc1e16bf5f2f2483374e848d2725cb2cf0f
unknown <mridulgupta2003official@gmail.com>
2023-02-18 20:20:26 +0530

    Profile Page

---
+++ src/Details/quizInfo.js
@@ -0,0 +1,41 @@
+const quizInfo = [
+    {
+        image: "./Images/Blockchain.svg",
+        publisher: "Polygon Technology",
+        questions: "10 questions",
+        time: "5 minutes",
+        reward: "1 ETH",
+        title: "What is Blockchain technology",
+        date_time: "20 February, 5:50 PM",
+        score: "1200",
+        rank: "15/50"
+
+
+    },
+    {
+        image: "./Images/English.svg",
+        publisher: "Oxford University",
+        questions: "20 questions",
+        time: "10 minutes",
+        reward: "1 ETH",
+        title: "English Grammar",
+        date_time: "25 February, 9:00 PM",
+        score: "1200",
+        rank: "15/50"
+
+    },
+    {
+        image: "./Images/Ethereum.svg",
+        publisher: "Ethereum Foundation",
+        questions: "20 questions",
+        time: "10 minutes",
+        reward: "5 ETH",
+        title: "What is Ethereum",
+        date_time: "28 February, 10:00 AM",
+        score: "1200",
+        rank: "15/50"
+
+    }
+]
+
+export default quizInfo
